ef89a09c88e23063f630e28ee345c6e84ae19e1b
minhyuk0914 <90814001+minhyuk0914@users.noreply.github.com>
2024-11-25 22:09:59 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,13 @@
+import sys
+
+sentence = sys.stdin.readline().split("-")
+
+total = 0
+for i in sentence[0].split('+'):
+    total += int(i)
+
+for i in sentence[1:]:
+    for j in i.split('+'):
+        total -= int(j)
+
+print(total)
