0535f918fe0fb2b72d842afff93da03c894affff
minhyuk0914 <90814001+minhyuk0914@users.noreply.github.com>
2024-12-26 15:53:51 +0900

    [Silver I] Title: IOIOI, Time: 336 ms, Memory: 35348 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -7,8 +7,19 @@ S = sys.stdin.readline()
 find_s = 'IOI' + 'OI' * (N-1)
 
 ans = 0
-for i in range(M):
-    if S[i : i + len(find_s)] == find_s:
-        ans += 1
-
+idx = 0
+cnt = 0
+while idx + 3 <= M:
+    if S[idx : idx + 3] == 'IOI': # IOI 패턴 찾기
+        idx += 2
+        cnt += 1
+        # IOI + 뒤에 IOI 가 나오는 횟수를 cnt
+        # cnt 가 N이라면, 우리가 찾는 IOI + OI * (N-1)
+        if cnt == N:
+            ans += 1
+            cnt -= 1 # 누적된 IOI 개수를 -1
+    
+    else:
+        idx += 1
+        cnt = 0
 print(ans)
