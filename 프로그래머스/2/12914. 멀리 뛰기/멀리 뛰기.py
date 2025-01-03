0d96c97241899ee48be32ecba34b98cf7ac94a50
minhyuk0914 <90814001+minhyuk0914@users.noreply.github.com>
2025-01-03 21:53:22 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.85 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    dp = [0] * (n + 1)
+    
+    for i in range(1, n + 1):
+        if i == 1:
+            dp[i] = 1
+        elif i == 2:
+            dp[i] = 2
+        elif i > 2:
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567      
+    return dp[n]
